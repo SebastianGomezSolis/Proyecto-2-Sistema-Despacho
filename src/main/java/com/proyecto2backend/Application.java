f084a1c360b3c369c507db4d3de0e558bce0db2d
SebastianGomez <sebasjose13@gmail.com>
2025-10-23 23:42:51 -0600

    Cambios bd

--- src/main/java/com/proyecto2backend/Application.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto2backend;
-
-public class Application {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,19 @@ module com.proyecto2backend {
     requires java.sql;
     requires com.zaxxer.hikari;
     requires java.logging;
+    requires javafx.graphics;
 
 
+    // Opens para JavaFX
     opens com.proyecto2backend to javafx.fxml;
+    opens com.proyecto2backend.controller to javafx.fxml;
+    opens com.proyecto2backend.model to javafx.fxml;
+
+
+    // Exports
     exports com.proyecto2backend;
+    exports com.proyecto2backend.controller;
+    exports com.proyecto2backend.model;
+    exports com.proyecto2backend.datos;
+    exports com.proyecto2backend.logic;
 }
